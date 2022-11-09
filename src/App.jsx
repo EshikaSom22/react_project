4bc0c8af195ce8738cadd7264fc29eb42bf8155d
eshika <eshika22som2003@gmail.com>
2022-11-09 23:08:55 +0530

    hm

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import { useState } from 'react'
 // import './index.css'
 let currentTime=new Date();
- currentTime=currentTime.getHours;
-
+ currentTime=currentTime.getHours();
+ 
+ let greeting="";
+ const cssStyle={}
+ if(currentTime>=1 && currentTime<12){
+ greeting="Good Morning";
+cssStyle.color='green';}
+ else if(currentTime>=12 && currentTime<19){
+ greeting="Good Afternoon";
+ cssStyle.color='orange';
+ }
+ else{ 
+ greeting='Good Night';
+ cssStyle.color="black";
+ }
 const contain={
   backgroundColor: 'rgba(130, 237, 201, 0.73)',
     width: '100 wh',
@@ -13,7 +26,7 @@ const contain={
 }
 const main1={
   backgroundColor: 'rgba(241, 213, 188, 0.948)',
-    width: '50%',
+    width: '20%',
     height: '40px',
     padding: '10px',
     fontWeight: 'bold',
@@ -31,7 +44,7 @@ justifyContent: 'center'
 const App = () => {
   return (
     <div className='container' style={contain}>
-      <span className='main' style={main1}>Hello Sir,{currentTime}</span>
+      <span className='main' style={main1}>Hello Sir,<span style={cssStyle}>{greeting}</span></span>
     </div>
   )
 }
